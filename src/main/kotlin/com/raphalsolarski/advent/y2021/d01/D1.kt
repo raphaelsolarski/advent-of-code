3fec849fa171eb9f42987878e722bba2618390ba
Rafał Solarski <solarski.rafal@gmail.com>
2022-11-19 11:14:02 +0100

    Kotlin + gradle upgrade

---
+++ src/main/kotlin/com/raphalsolarski/advent/y2021/d01/D1.kt
@@ -0,0 +1,11 @@
+package com.raphalsolarski.advent.y2021.d01
+
+class D1 {
+
+    companion object {
+        fun foo(): Unit {
+            //https://adventofcode.com/2021/day/1
+            println("foo")
+        }
+    }
+}
---
+++ src/test/kotlin/com/raphalsolarski/advent/y2021/d01/D1Test.kt
@@ -0,0 +1,12 @@
+package com.raphalsolarski.advent.y2021.d01
+
+import org.junit.jupiter.api.Test
+
+class D1Test {
+
+    @Test
+    internal fun test1() {
+        D1.foo()
+    }
+
+}
