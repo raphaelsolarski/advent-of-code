24aa87bc73b63d4751f435941541dce9c1973da8
Rafał Solarski <solarski.rafal@gmail.com>
2023-12-02 23:23:15 +0100

    D02/2023 star1

--- src/main/kotlin/com/raphalsolarski/advent/y2023/d02/D02.kt
+++ src/main/kotlin/com/raphalsolarski/advent/y2023/d02/D02.kt
@@ -1,6 +1,10 @@
 package com.raphalsolarski.advent.y2023.d02
 
 object D02 {
+    private const val MAX_RED = 12
+    private const val MAX_GREEN = 13
+    private const val MAX_BLUE = 14
+
     fun parseLine(line: String): Game {
         val splits = line.split(": ")
         val id = splits[0].substring(5).toInt()
@@ -16,6 +20,14 @@ object D02 {
         }
 
         return Game(id, sets)
+    }
+
+    fun testPossibility(game: Game): Boolean {
+        return game.showedSets.none { cubeSet ->
+            cubeSet.red > MAX_RED
+                    || cubeSet.green > MAX_GREEN
+                    || cubeSet.blue > MAX_BLUE
+        }
     }
 
     data class Game(val id: Int, val showedSets: List<CubeSet>)
--- src/test/kotlin/com/raphalsolarski/advent/y2023/d02/D02Test.kt
+++ src/test/kotlin/com/raphalsolarski/advent/y2023/d02/D02Test.kt
@@ -1,11 +1,24 @@
 package com.raphalsolarski.advent.y2023.d02
 
+import com.raphalsolarski.advent.utils.ParseUtils
 import com.raphalsolarski.advent.y2023.d02.D02.CubeSet
 import com.raphalsolarski.advent.y2023.d02.D02.Game
+import com.raphalsolarski.advent.y2023.d02.D02.parseLine
+import com.raphalsolarski.advent.y2023.d02.D02.testPossibility
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class D02Test {
+
+    private val exampleS1Data = listOf(
+        "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green",
+        "Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue",
+        "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        "Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+    )
+
+    private val realInput = ParseUtils.readLinesFromResource("/2023/02/input.txt", withEmptyLines = false)
 
     @Test
     fun parseTest() {
@@ -17,7 +30,24 @@ class D02Test {
                     CubeSet(0, 2, 0)
                 )
             ),
-            D02.parseLine("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
+            parseLine("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green")
         )
     }
+
+    @Test
+    fun exampleS1() {
+        val idSum = exampleS1Data.map(::parseLine)
+            .filter(::testPossibility)
+            .sumOf(Game::id)
+        assertEquals(8, idSum)
+    }
+
+    @Test
+    fun realS1() {
+        val idSum = realInput.map(::parseLine)
+            .filter(::testPossibility)
+            .sumOf(Game::id)
+        assertEquals(2449, idSum)
+    }
+
 }
